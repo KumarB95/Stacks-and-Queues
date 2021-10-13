6e8f01594208d51349ffa04720e0594d81e5c8dd
KumarB95 <kumarbalingal@gmail.com>
2021-10-14 02:33:11 +0530

    Refactored : Dequeue from begining

--- src/test/java/com/bridgelabz/MyStackAndQueueTest.java
+++ src/test/java/com/bridgelabz/MyStackAndQueueTest.java
@@ -2,6 +2,8 @@ package com.bridgelabz;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Queue;
+
 public class MyStackAndQueueTest {
     @Test
     public void given3NumbersCreatingStack(){
@@ -26,5 +28,16 @@ public class MyStackAndQueueTest {
         stackQueue.peek();
         stackQueue.pop();
         stackQueue.print();
+    }
+    @Test
+    public void given3NumbersEnqueueAndDequeue(){
+        System.out.println("Welcome to queue program");
+        Queue queue=new Queue();
+        queue.enque(15);
+        queue.enque(16);
+        queue.enque(17);
+        queue.print();
+        queue.dequeue();
+        queue.print();
     }
 }
