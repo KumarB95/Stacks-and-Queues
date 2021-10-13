39a6ef166eab93df831de48d950c926bfcd1f171
KumarB95 <kumarbalingal@gmail.com>
2021-10-14 02:04:23 +0530

    Refactored : Can peak and pop elements till stack gets Empty

--- src/test/java/com/bridgelabz/MyStackAndQueueTest.java
+++ src/test/java/com/bridgelabz/MyStackAndQueueTest.java
@@ -12,4 +12,19 @@ public class MyStackAndQueueTest {
         stackQueue.push(56);
         stackQueue.print();
     }
+    @Test
+    public void given3NumbersCreatingStackAndPeakAndPopTillStackGetsEmpty(){
+        System.out.println("Welcome to stack and queue program");
+        StackQueue stackQueue=new StackQueue();
+        stackQueue.push(70);
+        stackQueue.push(30);
+        stackQueue.push(56);
+        stackQueue.print();
+        stackQueue.peek();
+        stackQueue.push(34);
+        stackQueue.print();
+        stackQueue.peek();
+        stackQueue.pop();
+        stackQueue.print();
+    }
 }
